0790940e967adedacd1ae8a29fc400d7637011a4
NataliaChunikhina <krnata@yandex.ru>
2021-10-24 20:32:13 +0500

    fix code quality errors

--- src/index.js
+++ src/index.js
@@ -8,37 +8,43 @@ const goWelcome = () => {
 };
 
 const executeGameStep = (gameStep) => {
-  const [question, result] = gameStep();
-  console.log(question);
+  let count = 3;
+  let isOk = true;
+  while (count > 0) {
+    const [question, result] = gameStep();
+    console.log(question);
 
-  const answerUser = readlineSync.question('Your answer: ');
-  let answer = answerUser;
-  if (typeof result === 'number') answer = Number(answerUser);
+    const answerUser = readlineSync.question('Your answer: ');
+    let answer = answerUser;
+    if (typeof result === 'number') answer = Number(answerUser);
 
-  if (answer === result) {
-    console.log('Correct!');
-    return true;
+    if (answer === result) {
+      console.log('Correct!');
+      count -= 1;
+    } else {
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${result}'.`);
+      count = 0;
+      isOk = false;
+    }
   }
-  console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${result}'.`);
-  return false;
+
+  return isOk;
 };
 
 const startGame = (game) => {
   const userName = goWelcome();
 
+  if (!game) return;
+
   const [gameDescr, gameStep] = game();
+
   console.log(gameDescr);
 
-  let count = 3;
-  while (count > 0) {
-    const isOk = executeGameStep(gameStep);
-    if (isOk) {
-      count -= 1;
-      if (count === 0) console.log(`Congratulations, ${userName}!`);
-    } else {
-      count = 0;
-      console.log(`Let's try again, ${userName}!`);
-    }
+  const isOk = executeGameStep(gameStep);
+  if (isOk) {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    console.log(`Let's try again, ${userName}!`);
   }
 };
 
